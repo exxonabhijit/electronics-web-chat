c6c6fec2a463e4cdc5c8a361a266bc299db3b1e1
Abhijit Mhatre <abhijitm2599@gmail.com>
2023-02-05 14:56:23 +0530

    sign in and login pages done

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import React from "react";
 
 export default function SignUpPage() {
   return (
-    <div>SignUpPage</div>
-  )
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="d-flex align-items-center justify-content-center">
+            <div className="col-lg-4 col-12 col-sm-12 col-md-8">
+              <div className="card p-5 border-0 shadow-sm">
+                <div className="card-body">
+                  <form>
+                    <div className="mb-3">
+                      <div className="row">
+                        <div className="col-12 col-md-6 col-lg-6">
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label text-muted fw-normal"
+                          >
+                            First Name
+                          </label>
+                          <input
+                            type="text"
+                            aria-describedby="firstNameHelp"
+                            className="form-control border-0 bg-light"
+                            required
+                          />
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-6">
+                          <label
+                            htmlFor="exampleInputEmail1"
+                            className="form-label text-muted fw-normal"
+                          >
+                            Last Name
+                          </label>
+                          <input
+                            type="text"
+                            aria-describedby="LastNameHelp"
+                            className="form-control border-0 bg-light"
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="exampleInputEmail1"
+                        className="form-label text-muted fw-normal"
+                      >
+                        Email address
+                      </label>
+                      <input
+                        type="email"
+                        aria-describedby="emailHelp"
+                        className="form-control border-0 bg-light"
+                        required
+                      />
+                      <div id="emailHelp" className="form-text text-danger">
+                        Invalid username
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="exampleInputPassword1"
+                        className="form-label text-muted fw-normal"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control border-0 bg-light"
+                        id="exampleInputPassword1"
+                        aria-describedby="passwordHelp"
+                        required
+                      />
+                      <div id="passwordHelp" className="form-text text-danger">
+                        Invalid Password
+                      </div>
+                    </div>
+                    <div className="mb-3 form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="exampleCheck1"
+                        required
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="exampleCheck1"
+                      >
+                        Show Password
+                      </label>
+                    </div>
+                    <button type="submit" className="btn-standard ">
+                      Login
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

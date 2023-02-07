78610d2e2716159a39f734a9e7d0206a09c4d4a9
Abhijit Mhatre <abhijitm2599@gmail.com>
2023-02-07 17:48:26 +0530

    product detail code started

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import '../public/css/style-prefix.css';
 import '../public/css/style.css'
 
